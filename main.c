507f5de96698fdc9dcd8eb1c889762ebbd0fc6ac
Steve Lukis <stevelukis88@gmail.com>
2019-03-11 12:46:56 +0800

    matrix multi, optimized with transpose

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include<omp.h>
 #include<time.h>
 
-#define NUM_OF_THREADS 4
+#define NUM_OF_THREADS 2
 
 int arraySize = 1000;
 
@@ -11,12 +11,12 @@ int *arrA;
 int *arrB;
 int *arrC;
 
-int *init(int size) {
+int* init(int size) {
     int *arr = malloc(sizeof(int) * size * size);
     return arr;
 }
 
-int *randoms(int size) {
+int* randoms(int size) {
     int *arr = init(size);
     for (int i = 0; i < size * size; i++) {
         arr[i] = rand() % 100;
@@ -24,7 +24,7 @@ int *randoms(int size) {
     return arr;
 }
 
-int *zeros(int size) {
+int* zeros(int size) {
     int *arr = init(size);
     for (int i = 0; i < size * size; i++) {
         arr[i] = rand() % 100;
@@ -36,44 +36,64 @@ double dur(clock_t start, clock_t finish) {
     return ((double) (finish - start) / CLOCKS_PER_SEC);
 }
 
+double dur_linux(double start, double finish) {
+    return finish - start;
+}
+
 int main() {
     arrA = randoms(arraySize);
     arrB = randoms(arraySize);
     arrC = zeros(arraySize);
 
-    clock_t sStart, sFinish, pStart, pFinish;
+//    clock_t sStart, sFinish, pStart, pFinish;
+    double sStart, sFinish, pStart, pFinish;
+
+    //transpose array B
+    int* arrB_transpose = zeros(arraySize);
+#pragma omp for
+    for (int i = 0; i < arraySize; i++)
+        for(int j = 0 ; j < arraySize; j++)
+            arrB_transpose[arraySize * i + j] = arrB[arraySize * j + i];
 
     //serial
-    sStart = clock();
+//    sStart = clock();
+    sStart = omp_get_wtime();
     for (int i = 0; i < arraySize; i++) {
         for (int j = 0; j < arraySize; j++) {
             int sum = 0;
             for (int k = 0; k < arraySize; k++) {
-                sum += arrA[arraySize * i + k] * arrB[arraySize * k + j];
+                sum += arrA[arraySize * i + k] * arrB[arraySize * j + k];
             }
             arrC[arraySize * i + j] = sum;
         }
     }
-    sFinish = clock();
+//    sFinish = clock();
+    sFinish = omp_get_wtime();
 
-    //paralel
+    //parallel
     omp_set_num_threads(NUM_OF_THREADS);
-    pStart = clock();
+
+//    pStart = clock();
+    pStart = omp_get_wtime();
 
 #pragma omp parallel for
     for (int i = 0; i < arraySize; i++) {
         for (int j = 0; j < arraySize; j++) {
             int sum = 0;
             for (int k = 0; k < arraySize; k++) {
-                sum += arrA[arraySize * i + k] * arrB[arraySize * k + j];
+                sum += arrA[arraySize * i + k] * arrB[arraySize * j + k];
             }
             arrC[arraySize * i + j] = sum;
         }
     }
-    pFinish = clock();
+//    pFinish = clock();
+    pFinish = omp_get_wtime();
 
-    printf("Serial time = %f\n", dur(sStart, sFinish));
-    printf("Paralel time = %f\n", dur(pStart, pFinish));
+//    printf("Serial time = %f\n", dur(sStart, sFinish));
+//    printf("Paralel time = %f\n", dur(pStart, pFinish));
+
+    printf("Serial time = %fs\n", dur_linux(sStart, sFinish));
+    printf("Paralel time = %fs\n", dur_linux(pStart, pFinish));
 
     return 0;
 }

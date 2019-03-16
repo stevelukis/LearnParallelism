dce50651731b6a733b51b26d80c0c4d97c826958
Steve Lukis <stevelukis88@gmail.com>
2019-03-16 14:57:05 +0800

    fibonacci

--- main.c
+++ main.c
@@ -3,37 +3,61 @@
 #include<omp.h>
 #include<time.h>
 
-#define NUM_OF_THREADS 2
+#define NUM_OF_THREADS 4
 
-int arraySize = 1000;
+#define FIBONACCI_NUMBER 20
 
-int *arrA;
-int *arrB;
-int *arrC;
 
-int* init(int size) {
-    int *arr = malloc(sizeof(int) * size * size);
-    return arr;
-}
+long fib_3(int n) {
+    long x, y;
 
-int* randoms(int size) {
-    int *arr = init(size);
-    for (int i = 0; i < size * size; i++) {
-        arr[i] = rand() % 100;
+    if (n < 2) return n;
+    else {
+//        printf("Number of threads : %d\n", omp_get_thread_num());
+        x = fib_3(n - 1);
+        y = fib_3(n - 2);
+        return x + y;
     }
-    return arr;
 }
 
-int* zeros(int size) {
-    int *arr = init(size);
-    for (int i = 0; i < size * size; i++) {
-        arr[i] = rand() % 100;
+long fib_2(int n) {
+    long x, y;
+
+    if (n < 2) return n;
+    else {
+#pragma omp task shared(x)
+        {
+            printf("Number of threads : %d\n", omp_get_thread_num());
+            x = fib_3(n - 1);
+        }
+#pragma omp task shared(y)
+        {
+            printf("Number of threads : %d\n", omp_get_thread_num());
+            y = fib_3(n - 2);
+        }
+#pragma omp taskwait
+        return x + y;
     }
-    return arr;
 }
 
-double dur(clock_t start, clock_t finish) {
-    return ((double) (finish - start) / CLOCKS_PER_SEC);
+long fib_1(int n) {
+    long x, y;
+
+    if (n < 2) return n;
+    else {
+        #pragma omp task shared(x)
+        {
+            printf("Number of threads : %d\n", omp_get_thread_num());
+            x = fib_2(n - 1);
+        }
+        #pragma omp task shared(y)
+        {
+            printf("Number of threads : %d\n", omp_get_thread_num());
+            y = fib_2(n - 2);
+        }
+        #pragma omp taskwait
+        return x + y;
+    }
 }
 
 double dur_linux(double start, double finish) {
@@ -41,59 +65,29 @@ double dur_linux(double start, double finish) {
 }
 
 int main() {
-    arrA = randoms(arraySize);
-    arrB = randoms(arraySize);
-    arrC = zeros(arraySize);
-
-//    clock_t sStart, sFinish, pStart, pFinish;
     double sStart, sFinish, pStart, pFinish;
 
-    //transpose array B
-    int* arrB_transpose = init(arraySize);
-#pragma omp for
-    for (int i = 0; i < arraySize; i++)
-        for(int j = 0 ; j < arraySize; j++)
-            arrB_transpose[arraySize * i + j] = arrB[arraySize * j + i];
-
-    //serial
-//    sStart = clock();
-    sStart = omp_get_wtime();
-    for (int i = 0; i < arraySize; i++) {
-        for (int j = 0; j < arraySize; j++) {
-            int sum = 0;
-            for (int k = 0; k < arraySize; k++) {
-                sum += arrA[arraySize * i + k] * arrB[arraySize * j + k];
-            }
-            arrC[arraySize * i + j] = sum;
-        }
-    }
-//    sFinish = clock();
-    sFinish = omp_get_wtime();
-
-    //parallel
     omp_set_num_threads(NUM_OF_THREADS);
 
-//    pStart = clock();
-    pStart = omp_get_wtime();
+    sStart = omp_get_wtime();
+    long fibSerial = fib_3(FIBONACCI_NUMBER);
+    sFinish = omp_get_wtime();
 
-#pragma omp parallel for
-    for (int i = 0; i < arraySize; i++) {
-        for (int j = 0; j < arraySize; j++) {
-            int sum = 0;
-            for (int k = 0; k < arraySize; k++) {
-                sum += arrA[arraySize * i + k] * arrB[arraySize * j + k];
-            }
-            arrC[arraySize * i + j] = sum;
-        }
-    }
-//    pFinish = clock();
+    pStart = omp_get_wtime();
+    long fibParallel;
+#pragma omp parallel
+    {
+#pragma omp single
+        fibParallel = fib_1(FIBONACCI_NUMBER);
+    };
     pFinish = omp_get_wtime();
 
-//    printf("Serial time = %f\n", dur(sStart, sFinish));
-//    printf("Paralel time = %f\n", dur(pStart, pFinish));
+    printf("Serial result = %ld\n", fibSerial);
+    printf("Parallel result = %ld\n\n", fibParallel);
+
 
     printf("Serial time = %fs\n", dur_linux(sStart, sFinish));
-    printf("Paralel time = %fs\n", dur_linux(pStart, pFinish));
+    printf("Parallel time = %fs\n", dur_linux(pStart, pFinish));
 
     return 0;
 }

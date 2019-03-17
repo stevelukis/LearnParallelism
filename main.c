6348d0eaff9dff5d5ba44391b59da15d6670e86a
Steve Lukis <stevedesk01@gmail.com>
2019-03-17 09:57:38 +0800

    Revert "matrix multi, optimized with transpose"
    This reverts commit 507f5de9

--- main.c
+++ main.c
@@ -1,90 +1,79 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<omp.h>
+#include<time.h>
 
 #define NUM_OF_THREADS 4
 
-#define FIBONACCI_NUMBER 40
+int arraySize = 1000;
 
+int *arrA;
+int *arrB;
+int *arrC;
 
-long fib_3(int n) {
-    long x, y;
-
-    if (n < 2) return n;
-    else {
-        x = fib_3(n - 1);
-        y = fib_3(n - 2);
-        return x + y;
-    }
+int *init(int size) {
+    int *arr = malloc(sizeof(int) * size * size);
+    return arr;
 }
 
-long fib_2(int n) {
-    long x, y;
-
-    if (n < 2) return n;
-    else {
-#pragma omp task shared(x)
-        {
-            x = fib_3(n - 1);
-        }
-#pragma omp task shared(y)
-        {
-            y = fib_3(n - 2);
-        }
-#pragma omp taskwait
-        return x + y;
+int *randoms(int size) {
+    int *arr = init(size);
+    for (int i = 0; i < size * size; i++) {
+        arr[i] = rand() % 100;
     }
+    return arr;
 }
 
-long fib_1(int n) {
-    long x, y;
-
-    if (n < 2) return n;
-    else {
-        #pragma omp task shared(x)
-        {
-            x = fib_2(n - 1);
-        }
-        #pragma omp task shared(y)
-        {
-            y = fib_2(n - 2);
-        }
-        #pragma omp taskwait
-        return x + y;
+int *zeros(int size) {
+    int *arr = init(size);
+    for (int i = 0; i < size * size; i++) {
+        arr[i] = rand() % 100;
     }
+    return arr;
 }
 
-double dur_linux(double start, double finish) {
-    return finish - start;
+double dur(clock_t start, clock_t finish) {
+    return ((double) (finish - start) / CLOCKS_PER_SEC);
 }
 
 int main() {
-    double sStart, sFinish, pStart, pFinish;
+    arrA = randoms(arraySize);
+    arrB = randoms(arraySize);
+    arrC = zeros(arraySize);
 
+    clock_t sStart, sFinish, pStart, pFinish;
+
+    //serial
+    sStart = clock();
+    for (int i = 0; i < arraySize; i++) {
+        for (int j = 0; j < arraySize; j++) {
+            int sum = 0;
+            for (int k = 0; k < arraySize; k++) {
+                sum += arrA[arraySize * i + k] * arrB[arraySize * k + j];
+            }
+            arrC[arraySize * i + j] = sum;
+        }
+    }
+    sFinish = clock();
+
+    //paralel
     omp_set_num_threads(NUM_OF_THREADS);
+    pStart = clock();
 
-    printf("Serial in progress...");
+#pragma omp parallel for
+    for (int i = 0; i < arraySize; i++) {
+        for (int j = 0; j < arraySize; j++) {
+            int sum = 0;
+            for (int k = 0; k < arraySize; k++) {
+                sum += arrA[arraySize * i + k] * arrB[arraySize * k + j];
+            }
+            arrC[arraySize * i + j] = sum;
+        }
+    }
+    pFinish = clock();
 
-    sStart = omp_get_wtime();
-    long fibSerial = fib_3(FIBONACCI_NUMBER);
-    sFinish = omp_get_wtime();
-
-    printf("Parallel in progress...");
-
-    pStart = omp_get_wtime();
-    long fibParallel;
-#pragma omp parallel num_threads(NUM_OF_THREADS)
-    {
-#pragma omp single
-        fibParallel = fib_1(FIBONACCI_NUMBER);
-    };
-    pFinish = omp_get_wtime();
-
-    printf("Serial result = %ld\n", fibSerial);
-    printf("Parallel result = %ld\n\n", fibParallel);
-
-
-    printf("Serial time = %fs\n", dur_linux(sStart, sFinish));
-    printf("Parallel time = %fs\n", dur_linux(pStart, pFinish));
+    printf("Serial time = %f\n", dur(sStart, sFinish));
+    printf("Paralel time = %f\n", dur(pStart, pFinish));
 
     return 0;
 }
